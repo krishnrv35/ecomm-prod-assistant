577f2e1157ac4a14b5ac1c6f7a390de1ac5aa1c5
krishnrv35 <krishnrv35@gmail.com>
2025-10-13 15:30:38 -0400

    code updated

--- get_lib_versions.py
+++ get_lib_versions.py
@@ -1,13 +1,31 @@
 import importlib.metadata
 packages = [
-    "langchain",
-    "python-dotenv",
-    "langchain_core",
-    "streamlit",
-    "langgraph",
+    "beautifulsoup4",
+    "ddgs",
     "FASTAPI",
-    "streamlit"
+    "html5lib",
+    "jinja2",
+    "langchain",
+    "langchain-astradb",
+    "langchain-google-genai",
+    "langchain-groq",
+    "langchain-mcp-adapters",
+    "langchain-openai",
+    "langchain_core",
+    "langgraph",
+    "lxml",
+    "mcp",
+    "python-dotenv",
+    "python-multipart",
+    "pypdf",
+    "ragas",
+    "selenium",
+    "streamlit",
+    "structlog",
+    "undetected-chromedriver",
+    "uvicorn"
 ]
+
 for pkg in packages:
     try:
         version = importlib.metadata.version(pkg)
